9875fdb713350d959443812f9e601d9cab720686
Lucas Arantes <lucas.arantes@ligamagic.com>
2022-11-10 03:43:39 -0300

    feature: aula final

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"product-tax-microservice/internal/order/infra/database"
 	"product-tax-microservice/internal/order/usecase"
 	"product-tax-microservice/pkg/rabbitmq"
@@ -33,7 +34,25 @@ func main() {
 	out := make(chan amqp.Delivery) // cria o Channel
 	go rabbitmq.Consume(ch, out)    // Thread 2
 
-	for msg := range out {
+	qtdWorkers := 5
+	for i := 1; i <= qtdWorkers; i++ {
+		go worker(out, &uc, i) // Creating more threads
+	}
+
+	http.HandleFunc("/total", func(w http.ResponseWriter, r *http.Request) {
+		getTotalUC := usecase.GetTotalUseCase{OrderRepository: repository}
+		total, err := getTotalUC.Execute()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+		}
+		json.NewEncoder(w).Encode(total)
+	})
+	http.ListenAndServe(":8080", nil)
+}
+
+func worker(deliveryMessage <-chan amqp.Delivery, uc *usecase.CalculateFinalPriceUseCase, workerID int) {
+	for msg := range deliveryMessage {
 		var inputDTO usecase.OrderInputDTO
 
 		err := json.Unmarshal(msg.Body, &inputDTO)
@@ -45,8 +64,8 @@ func main() {
 			panic(err)
 		}
 		msg.Ack(false)
-		fmt.Println(outputDTO)
-		time.Sleep(50 * time.Millisecond)
+		fmt.Printf("Worker %d has processed order %s\n", workerID, outputDTO.ID)
+		time.Sleep(1 * time.Second)
 	}
 }
 
